4a976036c74b9de609b1036eb553249e8480d3a0
michelmouhani <michelmouhani.it@gmail.com>
2026-03-09 00:19:14 +0100

    Refactor CORS configuration and update bootstrap function call

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   // Optional CORS config if frontend on different domain
-  app.enableCors({
-    origin: 'https://latodo-xi.vercel.app', // your frontend URL
-    // origin: 'http://localhost:5173', // your frontend URL
-    credentials: true, // allow cookies to be sent
-  });
+  // app.enableCors({
+  //   // origin: 'https://latodo-xi.vercel.app', // your frontend URL
+  //   // origin: 'http://localhost:5173', // your frontend URL
+  //   // credentials: true, // allow cookies to be sent
+  // });
   // app.useGlobalPipes(
   //   new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   // );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
